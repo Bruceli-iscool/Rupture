46e524eda28894da0c479aa57427b99bbce4e69e
Bruce Li <isanoob1897@outlook.com>
2024-01-01 13:14:02 -0800

    added more functions

--- src/Rupture.py
+++ src/Rupture.py
@@ -1,26 +1,34 @@
 import random
 import tkinter as tk
-import turtle as tur
+import turtle
+
+turtle = turtle
+
 
 class Rupture:
     def __init__(self):
-        self.window = tk.Tk()
+        self.turtle = turtle
+        self.key_bindings = {}
+        self.game_running = True
 
     def write(self):
         print(self)
+        return
 
     def ask(self):
         user_input = input(self)
         return user_input
 
-    def ran(self, value):
+    def rad(self, value):
         return random.randint(0, value)
 
     def window_title(self, title):
         self.window.title(title)
+        return
 
     def window_size(self, x, y):
         self.window.geometry(f"{x}x{y}")
+        return
 
     def window_text(self, text):
         text_message = tk.Message(self.window, text=text, width=300)
@@ -33,12 +41,45 @@ class Rupture:
     def start(self):
         self.window.mainloop()
 
-    def file_open_read(self):
-        with open(self, 'r'):
-            pass
+    def size(self):
+        len(self)
+        return
 
-    def file_open_write(self):
-        with open(self, 'w'):
-            pass
+    def draw_window(self):
+        self.turtle.Screen()
 
+    def create_brush(self):
+        self.turtle.Turtle()
+        return
 
+    def brush_left(self, value):
+        self.turtle.left(value)
+        return
+
+    def brush_right(self, value):
+        self.turtle.left(value)
+        return
+
+    def brush_forward(self, value):
+        self.turtle.right(value)
+        return
+
+    def draw_close(self):
+        self.turtle.exitonclick()
+
+    def color(self):
+        turtle.color(self)
+
+    def draw_speed(self, speed):
+        self.turtle.speed(speed)
+
+    def pressed_key(self, event):
+        key = event.char.lower()
+        if key in self.key_bindings:
+            callback = self.key_bindings[key]
+            callback()
+        else:
+            print(f"Unknown key pressed: {key}")
+
+    def window(self):
+        self.window = tk.Tk()
--- src/rupture_example.py
+++ src/rupture_example.py
@@ -1,3 +1,9 @@
 from src.Rupture import Rupture
 rp = Rupture()
+rp.draw_window()
+rp.create_brush()
+rp.draw_speed(1)
+rp.brush_left(1)
+rp.brush_forward(2000000)
 
+rp.draw_close()
