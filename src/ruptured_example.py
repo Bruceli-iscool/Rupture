d2c5917900eef31819e1cd45e9385875bcd4a082
Bruce Li <isanoob1897@outlook.com>
2024-01-05 00:46:11 +0000

    add example

--- src/ruptured_example.py
+++ src/ruptured_example.py
@@ -1,7 +1,32 @@
 from src.Ruptured import Ruptured
 
 rp = Ruptured()
-x = rp.ask('What\'s your age?')
-rp.convert(x, int)
-rp.write(x)
+rp.draw_window
+rp.create_brush
+rp.brush_name('ArtBoard')
+def moveup():
+    rp.brush_up(20)
+
+def movedown():
+    rp.brush_down()
+
+def moveleft():
+    rp.brush_left
+
+def moveright():
+    rp.brush_right
+
+def clearboard():
+    rp.brush_clear
+
+rp.color('black')
+
+rp.brush_key(clearboard, 'c')    
+rp.brush_key(moveright, 'd')
+rp.brush_key(moveleft, 'a')
+rp.brush_key(movedown, 's')
+rp.brush_key(moveup, 'w')
+rp.draw_close()
+
+
 
